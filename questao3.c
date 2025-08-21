38318adf782d1c672995fd44a2f6373dd3a5b246
William6002 <williamgoncalveslopes58@gmail.com>
2025-08-20 21:39:34 -0300

    questao3.c

--- questao3.c
+++ questao3.c
@@ -31,24 +31,27 @@ int calculo(int array[], int valor_procurado, int size) {
     return resultado;
 }
 int main() {
-    int idades[5];
-    int notas[5];
-    for (int i = 0; i < 5; i++) {
-        printf("Insira a idade do cliente %d (total de 5 clientes): ", i + 1);
+    int n1;
+    printf("quantos clientes serao registrados?");
+    scanf("%d", &n1);
+    int idades[n1];
+    int notas[n1];
+    for (int i = 0; i < n1; i++) {
+        printf("Insira a idade do cliente %d (total de %d clientes): ", i + 1, n1);
         scanf("%d", &idades[i]);
-        printf("Insira a nota do cliente %d (total de 5 clientes)(3 - excelente, 2 - bom, 1 - regular): ", i + 1);
+        printf("Insira a nota do cliente %d (total de %d clientes)(3 - excelente, 2 - bom, 1 - regular): ", i + 1, n1);
         scanf("%d", &notas[i]);
     }
 
-    int opiniao1 = calculo(notas, 1, 5);
-    int opiniao2 = calculo(notas, 2, 5);
-    int opiniao3 = calculo(notas, 3, 5);
+    int opiniao1 = calculo(notas, 1, n1);
+    int opiniao2 = calculo(notas, 2, n1);
+    int opiniao3 = calculo(notas, 3, n1);
     printf("Total de clientes que deram nota 1: %d\n", opiniao1);
     printf("Total de clientes que deram nota 2: %d\n", opiniao2);
     printf("Total de clientes que deram nota 3: %d\n", opiniao3);
-    float media1 = media(notas, idades, 5, 1);
-    float media2 = media(notas, idades, 5, 2);
-    float media3 = media(notas, idades, 5, 3);
+    float media1 = media(notas, idades, n1, 1);
+    float media2 = media(notas, idades, n1, 2);
+    float media3 = media(notas, idades, n1, 3);
     printf("A media de idade dos clientes que deram nota 1 foi: %.2f\n", media1);
     printf("A media de idade dos clientes que deram nota 2 foi: %.2f\n", media2);
     printf("A media de idade dos clientes que deram nota 3 foi: %.2f\n", media3);
@@ -58,5 +61,6 @@ int main() {
     printf("%.2f%% dos clientes responderam 1, %.2f%% responderam 2 e %.2f%% responderam 3\n", porcentagem1, porcentagem2, porcentagem3);
 
     system("PAUSE");
+
 
 }
